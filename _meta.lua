e630f997fdcdca39d03724bbac3ceb10a63ba6a4
Tomas Di Leo <tomasjdle@gmail.com>
2026-01-12 16:17:35 -0300

    Updated metadata

--- _meta.lua
+++ _meta.lua
@@ -5,5 +5,5 @@ return {
     name = "pageturner",
     fullname = _("Page Turner"),
     description = _("A Remote Page Turner that connect to a companion App to satisfy your page turning hunger for free!"),
-    version = "1.0.0",
+    version = "1.2.0",
 }
